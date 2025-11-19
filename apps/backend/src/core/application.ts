3bd9bbf73c19b3f254982d4f43e85fd07dcafd4d
Michal Cieslar <michalovskyyy@gmail.com>
2025-11-19 20:33:08 +0100

    chore: cleanup application class

--- apps/backend/src/core/application.ts
+++ apps/backend/src/core/application.ts
@@ -14,27 +14,21 @@ export class Application {
 
   public static async start(): Promise<void> {
     const config = createConfig();
-
     const loggerService = LoggerServiceFactory.create({ logLevel: config.logLevel });
-
     this.databaseClient = new DatabaseClient(config.database, loggerService);
-
-    await this.databaseClient.testConnection();
-
     const emailRepository = new EmailRepositoryImpl(this.databaseClient);
     const emailService = new EmailServiceImpl(config);
     this.emailProcessingService = new EmailProcessingService(emailRepository, emailService, loggerService);
-
-    this.emailProcessingService.start();
-
     this.server = new HttpServer(config, loggerService, this.databaseClient);
 
+    await this.databaseClient.testConnection();
+    this.emailProcessingService.start();
     await this.server.start();
   }
 
   public static async stop(): Promise<void> {
-    this.emailProcessingService?.stop();
     await this.server?.stop();
+    this.emailProcessingService?.stop();
     await this.databaseClient?.close();
   }
 }
