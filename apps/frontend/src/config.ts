314a66c35221cb107c0f79e03b1e1567c42b95ff
Michal Cieslar <michalovskyyy@gmail.com>
2025-11-23 14:59:25 +0100

    chore: remove debug log

--- apps/frontend/src/config.ts
+++ apps/frontend/src/config.ts
@@ -45,8 +45,6 @@ export type Config = z.infer<typeof configSchema>;
 export function createConfig(): Config {
   const parsedConfig = configSchema.safeParse(appConfig);
 
-  console.log('App Config:', appConfig);
-
   if (!parsedConfig.success) {
     logger.error(parsedConfig.error);
     throw new Error('Configuration error');
